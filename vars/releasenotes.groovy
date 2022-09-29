2c2630852a19674a065676f23234c075bf1739a7
dpapasp <74298024+dpapasp@users.noreply.github.com>
2022-09-29 10:31:24 +0300

    Update releasenotes.groovy

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -23,8 +23,24 @@ def call(Map config=[:]){
     def date = new Date();
     def sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
     echo "Date and Time IS: " + sdf.format(date);
+   
     
     echo "Build Number is: ${BUILD_NUMBER}"
+    
+    
+    def changeLogSets = currentBuild.changeSets;
+
+		for (change in changeLogSets) {
+			def entries = change.items;
+			for (entry in entries) {
+				echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}"
+				for (file in entry.affectedFiles) {
+					echo "  ${file.editType.name} ${file.path}";
+				}
+			}
+		}
+		
+    
     if(config.changes != "false"){
         echo "changes";
     }
